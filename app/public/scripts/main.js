aa533362b22deabbb1c349d0dced782386668fd4
mmichaud93 <mmichaud93@gmail.com>
2015-04-07 22:36:15 -0400

    search

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var typeAheadTimeout = 0;
 $(document).ready(function() {
   $('#splash').backstretch('images/splash-fun.jpg');
   
@@ -9,7 +9,8 @@ $(document).ready(function() {
     {h1: 'Understanding code.', small:'Best. Feeling. Ever.'},
     {h1: 'Zero downtime.', small:'Now you\'re talking.'},
     {h1: 'Exception handling.', small:'Let us do that for you.'},
-    {h1: 'Arbitrary data.', small:'Log whatever feels right.'}
+    {h1: 'Arbitrary data.', small:'Log whatever feels right.'},
+    {h1: 'Ben smells', small:'... good'}
   ], prevTagline = 0;
 
   setInterval(function() {
@@ -24,7 +25,31 @@ $(document).ready(function() {
       $('#splash div h1').text(taglines[index].h1).append('<small>'+taglines[index].small+'</small>').fadeIn('slow');
     });
   }, 10000);
+  
+  $('.repo-search').keydown(function(e) {
+    typeAheadTimeout = new Date().getTime()+300;
+    
+    setTimeout(function() {
+      if(new Date().getTime() < typeAheadTimeout) {
+      } else {
+        search(e);
+      }
+    }, 300);
+  });
 });
+
+function search(e) {
+  $.get( "repo-search/?q="+$('.repo-search').val(), function( data ) {
+    // remove all things from the table
+    $('.repos-body').find('tr').remove();
+    for(var i = 0; i < data.repos.length; i++) {
+      var repo = data.repos[i];
+      console.log(repo);
+      $('.repos-body').append('<tr><td class="name"><a href="/'+repo.fullName+'">'+repo.fullName+'</a></td></tr>');
+    }
+    console.log(data);
+  });
+}
 
 var TableView = function(selector) { // jshint ignore:line
     this.$el = $(selector);
